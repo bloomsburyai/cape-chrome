08bcfbdd036f78739b492373cfea9b4a6d116796
Michael Sheldon <mike@bloomsbury.ai>
2018-01-04 12:10:00 +0000

    Initial commit

---
+++ cape.js
@@ -0,0 +1,99 @@
+$(function () {
+    var text_question = '';
+    var total_ans = 0;
+    var default_loading_response = '<i class="fa fa-spinner fa-pulse"></i>';
+    var processing_container = $('.response-holder');
+    var button_select = $('.btn-select ');
+    button_select.addClass('btn-select-disabled');
+    processing_container.html(default_loading_response);
+    processing_container.hide();
+    var turndown = new TurndownService();
+    turndown.remove("script");
+    turndown.remove("link");
+    turndown.remove("meta");
+    turndown.remove("style");
+    var markdown;
+    var timer = 0;
+    var tab_id = 0;
+
+	var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = false;
+	recognition.onstart = function() { console.log("started recognition"); }
+    recognition.onresult = function(event) { console.log(event) }
+    recognition.start();
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        console.log(tabs);
+        tab_id = tabs[0].id;
+        chrome.tabs.sendMessage(tab_id, {'command': 'convert'}, parse_html);
+    });
+
+    function parse_html(html) {
+        markdown = turndown.turndown(html).substr(0, 6789);
+    }
+        
+    function ask_question(question_input) {
+        text_question = question_input.val();
+        processing_container.html(default_loading_response);
+        processing_container.show();
+        // Only submit question if the user hasn't been typing for half a second
+        if (timer != 0) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+            $.post('https://responder.thecape.ai/api/0.1/answer?token=hj1-BZfR7IICCXtVHZXeEeiZdfXkVeDjnZnzZ3HcR_o',
+                {
+                    'text': markdown,
+                    'question': text_question,
+                    'sourceType': 'document'
+                }, function (reply) {
+                    total_ans = reply['result']['items'].length;
+                    if (total_ans > 0) {
+                        answer = reply['result']['items'][0]['answerText'];
+                        chrome.tts.speak(answer);
+                        answer = answer.replace("\n", "").replace("  ", " ");
+                        chrome.tabs.sendMessage(tab_id, {'command': 'highlight', 'text': answer}, function() {});
+                    }
+                })
+                .done(function () {
+                    if (total_ans > 0) {
+                        processing_container.html('1/' + total_ans);
+                        processing_container.show();
+                        button_select.removeClass('btn-select-disabled');
+                    } else {
+                        processing_container.hide();
+                        button_select.addClass('btn-select-disabled');
+                    }
+                })
+        }, 500);
+    }
+        
+    $(".question-text").keyup(function() {
+        var question_input = $(this);
+        ask_question(question_input);
+        return false;
+    });
+        
+    $(".btn-select-disabled").click(function() {
+        return false;
+    });
+        
+    $(".select-prev").click(function() {
+        selected_ans--;
+        if (selected_ans < 0) {
+            selected_ans = total_ans - 1;
+        }
+        processing_container.html((selected_ans + 1) + '/' + total_ans);
+        processing_container.show();
+    });
+
+    $(".select-next").click(function() {
+        selected_ans++;
+        if (selected_ans >= total_ans) {
+            selected_ans = 0;
+        }
+        processing_container.html((selected_ans + 1) + '/' + total_ans);
+        processing_container.show();
+    });
+});
---
+++ content.js
@@ -0,0 +1,25 @@
+var capeMark = new Mark(document.querySelector("body"));
+var capeTopOffset = 250;
+
+chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
+    if (msg['command'] == 'convert') {
+        callback(document.querySelector("body").outerHTML);
+    } else if (msg['command'] == 'highlight') {
+        text = msg['text'];
+		capeMark.unmark({
+        	done: function(){
+        		capeMark.mark(text, {
+                    separateWordSearch: false,
+                    acrossElements: true,
+                    done: function() {
+						var element = document.querySelector("mark");
+						if (element !== null) {
+							element.scrollIntoView();
+                            window.scrollTo(window.scrollX, window.scrollY - capeTopOffset);
+						}
+                    }
+                });
+		    }
+		});
+    }
+});
