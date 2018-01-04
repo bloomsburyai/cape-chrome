1716dfb7314bc80c4639da5db5aad3b09da64eda
Michael Sheldon <mike@bloomsbury.ai>
2018-01-04 16:52:25 +0000

    Fix formatting

--- content.js
+++ content.js
@@ -12,21 +12,21 @@ chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
         }
     } else if (msg['command'] == 'highlight') {
         text = msg['text'];
-		capeMark.unmark({
-        	done: function(){
-        		capeMark.mark(text, {
+        capeMark.unmark({
+            done: function(){
+                capeMark.mark(text, {
                     separateWordSearch: false,
                     acrossElements: true,
                     done: function() {
-						var element = document.querySelector("mark");
-						if (element !== null) {
-							element.scrollIntoView();
+                        var element = document.querySelector("mark");
+                        if (element !== null) {
+                            element.scrollIntoView();
                             window.scrollTo(window.scrollX, window.scrollY - capeTopOffset);
-						}
+                        }
                     }
                 });
-		    }
-		});
+            }
+        });
     } else if (msg['command'] == 'clear') {
         capeMark.unmark();
     }
